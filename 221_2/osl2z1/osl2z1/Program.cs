93339a350e810ccbd30152cfbfed6e60a2bd2f12
Publo <111525871+Candymanzz@users.noreply.github.com>
2024-11-17 01:01:12 +0300

    lab2

---
+++ 221_2/osl2z1/osl2z1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Globalization;
+
+namespace osl2z1
+{
+    class Progarm
+    {
+        public static void Main()
+        {
+            string strParams = Console.ReadLine()!;
+            string[] arrStrParams = strParams.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            if (arrStrParams.Length != 4)
+            {
+                Console.WriteLine("Неверный формат входных данных. Пожалуйста, введите 4 числа через пробел.");
+                return;
+            }
+
+            double A = double.Parse(arrStrParams[0], CultureInfo.InvariantCulture);
+            double l = double.Parse(arrStrParams[1], CultureInfo.InvariantCulture);
+            double B = double.Parse(arrStrParams[2], CultureInfo.InvariantCulture);
+            double i = double.Parse(arrStrParams[3], CultureInfo.InvariantCulture);
+
+            Z1(A, l, B, i);
+        }
+
+        public static void Z1(double A, double l, double B, double i)
+        {
+            double sum = 0;
+            for (double j = l; j <= B; j++)
+            {
+                sum += Math.Sqrt(A) + i;
+            }
+            // Выводим только числовое значение
+            Console.WriteLine(sum);
+        }
+    }
+}
---
+++ 221_2/osl2z2/osl2z2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Globalization;
+
+namespace osl2z2
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            var param = Console.ReadLine()!;
+
+            // Теперь ожидаем только число
+            double s = double.Parse(param.Replace(",", "."), CultureInfo.InvariantCulture);
+            Z2(s);
+        }
+
+        public static void Z2(double s)
+        {
+            Console.WriteLine($"Результат: {Math.Pow(s, 3)}");
+        }
+    }
+}
